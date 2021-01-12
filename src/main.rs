fb81037da6aac3c565401c3ca614f0bd1f979870
francoposa <franco@francoposa.io>
2021-01-12 15:05:33 -0600

    Select 1 working with actix_rt and sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use sqlx::postgres::PgPoolOptions;
+
+#[actix_rt::main]
+async fn main() -> Result<(), sqlx::Error> {
     println!("Hello, world!");
+
+    let pool = PgPoolOptions::new()
+        .connect("postgres://postgres@localhost/authentication_identity_user_mgmt")
+        .await?;
+
+    #[derive(Debug)]
+    struct Select1Result {
+        result: Option<i32>,
+    };
+
+    let result = sqlx::query_as!(Select1Result, "SELECT 1 AS result")
+        .fetch_one(&pool)
+        .await?;
+
+    println!("{:?}", result.result);
+
+    Ok(())
 }
