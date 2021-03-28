9c8b744410e082ab022a18609ff04911f52223b8
francoposa <franco@francoposa.io>
2021-03-28 16:52:03 -0500

    adding username validation and errors

--- authentication/src/domain/user.rs
+++ authentication/src/domain/user.rs
@@ -1,5 +1,9 @@
 use chrono::{DateTime, Utc};
+use std::fmt;
 use uuid::Uuid;
+
+pub const MIN_USERNAME_LENGTH: usize = 8;
+pub const MAX_USERNAME_LENGTH: usize = 64;
 
 #[derive(Debug)]
 pub struct User {
@@ -21,6 +25,24 @@ impl User {
             enabled: true,
             created_at: now,
             updated_at: now,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum UsernameInvalidError {
+    UsernameInvalidLengthError {},
+    UsernameInvalidError { message: String },
+}
+
+impl fmt::Display for UsernameInvalidError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *&self {
+            UsernameInvalidError::UsernameInvalidLengthError {} => f.write_str(&format!(
+                "Username must be between {} and {} characters",
+                MIN_USERNAME_LENGTH, MAX_USERNAME_LENGTH
+            )),
+            UsernameInvalidError::UsernameInvalidError { message } => f.write_str(&message),
         }
     }
 }
